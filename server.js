af8f32d111fb346850b0e8ba07f228f0291e655a
nipunaupeksha <nipuna.upeksha@gmail.com>
2022-06-18 18:23:09 +0530

    adding functionalities

--- server.js
+++ server.js
@@ -1,26 +1,60 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var messages = [
-    { name: 'Bob', message: 'Hello' },
-    { name: 'Anna', message: 'Hi' },
-    { name: 'Emma', message: 'Hi, how are you?' }
-];
+mongoose.Promise = Promise;
+var dbUrl = 'mongodb://user:user@d515542.mlab.com:55424/learning-node';
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+});
 
 app.get("/messages", (req, res) => {
-    res.send(messages);
+    Message.find({}, (err, messages) => {
+        res.send(messages);
+    });
 });
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body);
-    res.sendStatus(200);
+    var message = new Message(req.body);
+
+    message.save()
+        .then(() => {
+            console.log("saved")
+            return Message.findOne({ message: 'badword' });
+        })
+        .then(censored => {
+            if (censored) {
+                console.log("censored words found", censored);
+                Message.remove({ __id: censored.id }, (err) => {
+                    console.log("removed censored message");
+                });
+            }
+            io.emit('message', req.body);
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            res.sendStatus(500);
+            return console.log(err);
+        });
 });
 
-var server = app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongo db connection', err);
+})
+
+var server = http.listen(3000, () => {
     console.log("server is listening on port ", server.address().port)
 });
